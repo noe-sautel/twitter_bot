75e6e49db53ccbaf9cb8b99d7b5ae1e853e83d8e
Noé <noesautel@iclloud.com>
2021-11-24 18:27:10 +0100

    ADD log

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tweepy
 import logging
 import emoji
-import config
+import config_creds as config
 import time
 import textgears
 import re
@@ -10,49 +10,54 @@ import requests
 from io import BytesIO
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)s [%(levelname)s] %(message)s", 
+    handlers=[
+        logging.FileHandler("main.log"),
+        logging.StreamHandler()
+    ]
+)
 logger = logging.getLogger()
 
-auth_user_list = ['ohmyxan', 'noesautel']
-not_follow_self = ['narcoticsqueed', 'noesautel']
+auth_user_list = ['ohmyxan']
+not_follow_self = ['narcoticsqueed', 'noesautel', 'ohmyxan_nemesis']
 
-def check_mentions(api, since_id):
+def check_mentions(api, keywords, since_id):
     logger.info("Retrieving mentions")
     new_since_id = config.since_id_read()
 
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id = since_id).items():
         new_since_id = max(tweet.id, new_since_id)
-        print([tweet.text, tweet.user.name, tweet.id, tweet.in_reply_to_status_id])
+        logging.info(f"CURSOR : text:{tweet.text}, user.name:{tweet.user.name}, id:{tweet.id}, in_reply_to_status_id:{tweet.in_reply_to_status_id}")
         if not tweet.in_reply_to_status_id:
             continue
-        elif api.get_status(tweet.in_reply_to_status_id).user.screen_name not in auth_user_list :
+        elif api.get_status(tweet.in_reply_to_status_id).user.screen_name not in auth_user_list and any(keyword in tweet.text for keyword in keywords):
             try :
                 api.update_status(status=f"Désolé, mais je ne corrige que les tweets de @ohmyxan {emoji.emojize(':blue_heart:')}", in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
-                logger.info(f"Answered to {tweet.user.name} - @ohmyxan filter")
-                print(f"At {tweet.id}")                
+                logger.info(f"Answered to {tweet.user.name} on {tweet.id} with @ohmyxan filter")       
             except tweepy.errors.HTTPException as error:
                 if error.api_codes == [187]: # tweepy.errors.Forbidden: 403 Forbidden 187 - Status is a duplicate
-                    logger.info("Duplicate tweet")
+                    logger.warning(f"Duplicate tweet {tweet.id}.")
                     continue
                 else:
                     raise error
-        elif api.get_status(tweet.in_reply_to_status_id).user.screen_name in auth_user_list :
+        elif api.get_status(tweet.in_reply_to_status_id).user.screen_name in auth_user_list and any(keyword in tweet.text for keyword in keywords):
             tweet_typo_prepared = api.get_status(tweet.in_reply_to_status_id).text
             tweet_typo = ' '.join(re.sub("(@[A-Za-z0-9_À-ÿ]+)|([^0-9A-Za-z_À-ÿ \t])|(\w+:\/\/\S+)"," ", tweet_typo_prepared).split())
             tweet_corrected = textgears.correct_text(tweet_text = tweet_typo, api_key = config.textgears_api())
             try :
                 api.update_status(status=tweet_corrected, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
-                logger.info(f"Answered to {tweet.user.name}")
-                print(f"At {tweet.id}: {tweet_corrected}")                
+                logger.info(f"Answered to {tweet.user.name} on {tweet.id}: {tweet_corrected}.")                
             except tweepy.errors.HTTPException as error:
                 if error.api_codes == [187]: # tweepy.errors.Forbidden: 403 Forbidden 187 - Status is a duplicate
-                    logger.info("Duplicate tweet")
+                    logger.warning(f"Duplicate tweet {tweet.id}")
                     continue
                 else:
                     raise error
         if not tweet.user.following and tweet.user.screen_name not in not_follow_self:
             tweet.user.follow()
-            logger.info(f"user @{tweet.user.name} has just been followed" )
+            logger.info(f"User @{tweet.user.name} has just been followed" )
 
         config.since_id_write(current_since_id=new_since_id)
         return new_since_id
@@ -68,10 +73,10 @@ def invert_image(api, user):
         inv_img.save('profile_picture.jpg')
         im2 = Image.open('profile_picture.jpg')
         if list(im1.getdata()) == list(im2.getdata()):
-            logger.info("Profile picture identical ; no update.")
+            logger.warning("Profile picture identical ; no update.")
         else:
             api.update_profile_image('profile_picture.jpg')
-            logger.info("Profile picture updated")
+            logger.info("Profile picture updated.")
         return None
     except Exception as e:
         logging.debug(e)    
@@ -80,7 +85,7 @@ def main():
     api = config.create_api()
     since_id = config.since_id_read()
     while True:
-        check_mentions(api=api, since_id=since_id)
+        check_mentions(api=api, keywords =["ohmyxan_nemesis"], since_id=since_id)
         invert_image(api=api, user='ohmyxan')
         logger.info("Waiting...")
         time.sleep(60)
