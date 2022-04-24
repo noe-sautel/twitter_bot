0dae9c3222b1257a2bca8fa6d3aad88911c7c6ee
Noé <noesautel@icloud.com>
2022-04-24 15:03:32 -0400

    UPD textgear and RESET previous commit

--- textgears.py
+++ textgears.py
@@ -31,16 +31,14 @@ def correct_text(tweet_text, api_key):
                 f"""textgears.correct_text error_code: {pretty_json["error_code"]} description: {pretty_json["description"]}"""
             )
             return f"Je n'ai plus de crédits pour accéder à mon dictionnaire. Essaie une autre fois ou contacte @noesautel {emoji.emojize(':technologist_medium_skin_tone:')}"
-            return None
         if not pretty_json["response"]["errors"]:
             return (
                 f"Aucune erreur trouvée. Si il y'en a une, vous pouvez"
                 f" me DM : @noesautel "
                 f"{emoji.emojize(':technologist_medium_skin_tone:')}"
             )
-        else:
-            description = pretty_json["response"]["errors"][0]["description"]["en"]
-            better = pretty_json["response"]["errors"][0]["better"][0]
-            return f"""{description} {emoji.emojize(':right_arrow:')} \"{better}\""""
+        description = pretty_json["response"]["errors"][0]["description"]["en"]
+        better = pretty_json["response"]["errors"][0]["better"][0]
+        return f"""{description} {emoji.emojize(':right_arrow:')} \"{better}\""""
     except Exception as e:
         logger.warning(f"textgears.correct_text error: {e}")
